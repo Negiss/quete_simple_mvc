653f73500c7b3c7953f7af8897f878c75bd30108
Jules Daniel <julesdaniel.3@gmail.com>
2018-10-11 11:49:38 +0200

    *tssh* MVC4 - Done! *over*

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -8,19 +8,31 @@
 
 namespace Controller;
 
-use Model;
+use Model\CategoryManager;
+use Twig_Loader_Filesystem;
+use Twig_Environment;
 
 class CategoryController
 {
-    public function index(){
-        $categoryManager = new Model\CategoryManager();
-        $category = $categoryManager->selectAllCategories();
-        require __DIR__ . '/../View/category.php';
+    private $twig;
+
+    public function __construct()
+    {
+        $loader = new Twig_Loader_Filesystem(__DIR__ . '/../View');
+        $this->twig = new Twig_Environment($loader);
     }
 
-    public function show(int $id){
-        $categoryManager = new Model\CategoryManager();
+    public function index()
+    {
+        $categoryManager = new CategoryManager();
+        $category = $categoryManager->selectAllCategories();
+        return $this->twig->render('category.html.twig', ['category' => $category]);
+    }
+
+    public function show(int $id)
+    {
+        $categoryManager = new CategoryManager();
         $showCategory = $categoryManager->selectOneCategory($id);
-        require __DIR__ . '/../View/showCategory.php';
+        return $this->twig->render('showCategory.html.twig', ['showCategory' => $showCategory]);
     }
 }
--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -4,21 +4,32 @@ namespace Controller;
 
 // src/Model/ItemManager.php
 
-use Model;
+use Model\ItemManager;
+use Twig_Loader_Filesystem;
+use Twig_Environment;
 
-class ItemController {
+class ItemController
+{
 
-    public function index(){
-        $itemManager = new Model\ItemManager();
-        $items = $itemManager->selectAllItems();
-        require __DIR__ . '/../View/item.php';
+    private $twig;
+
+    public function __construct()
+    {
+        $loader = new Twig_Loader_Filesystem(__DIR__.'/../View');
+        $this->twig = new Twig_Environment($loader);
     }
 
-    public function show(int $id){
-        $itemManager = new Model\ItemManager();
+    public function index()
+    {
+        $itemManager = new ItemManager();
+        $items = $itemManager->selectAllItems();
+        return $this->twig->render('item.html.twig', ['items' => $items]);
+    }
+
+    public function show(int $id)
+    {
+        $itemManager = new ItemManager();
         $item = $itemManager->selectOneItem($id);
-        require __DIR__ . '/../View/showItem.php';
+        return $this->twig->render('showItem.html.twig', ['item' => $item]);
     }
 }
-
-?>
